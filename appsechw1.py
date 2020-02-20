499d040d136ad1b36a686cedfe29e5fa48eca7fb
Hansen Chen <hansen487@gmail.com>
2020-02-19 19:25:01 -0500

    program that converts image to a square

---
+++ appsechw1.py
@@ -0,0 +1,17 @@
+from PIL import Image
+filepath = input("Enter a file path for the picture:")
+
+im = Image.open(filepath)
+print(im.info)
+width, height = im.size[0], im.size[1]
+if width > height:
+    width = im.size[1]
+else:
+    height = im.size[0]
+print(width, height)
+im_resized = im.resize((width, height))
+new_filepath = filepath.split('.')
+new_filepath = new_filepath[0] + "square." + new_filepath[1]
+print(new_filepath)
+im_resized.save(new_filepath)
+im.close()
